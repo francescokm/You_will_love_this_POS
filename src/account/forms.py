434c4b1ba0da5134b1a8b94e0b9d3535b828227e
Shawki Aladdin <bolshuq@gmail.com>
2021-11-06 09:19:27 -0400

    moving the module folder out of the src

--- src/account/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm, fields, models
-from django.contrib.auth.forms import UserCreationForm  
-from .models import Account
-
-class NewAccountForm(UserCreationForm):
-    class Meta:
-        model   =   Account
-        fields  =   '__all__'
-        exclude = ('is_active','is_admin','is_staff','is_superuser','password','last_login')
